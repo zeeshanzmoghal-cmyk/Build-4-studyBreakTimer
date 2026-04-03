9f21d5bc2f7076478ad2985b8cf5944d0d77d30b
Zeeshan Zahid Moghal <zeeshanzmoghal@gmail.com>
2026-04-03 10:38:17 -0400

    FIXED COMMIT

--- studyBreakTimer/Form1.cs
+++ studyBreakTimer/Form1.cs
@@ -1,145 +1,188 @@
+using System.Diagnostics;
+
 namespace studyBreakTimer
 {
-    partial class Form1
+    public partial class Form1 : Form
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        public Form1()
         {
-            if (disposing && (components != null))
+            InitializeComponent();
+        }
+
+        private Stopwatch stopWatch = new Stopwatch();
+        private bool isFiveMinSelected = false;
+        private bool isTenMinSelected = false;
+        private bool isFifteenMinSelected = false;
+
+        private void btnFiveMin_Click(object sender, EventArgs e)
+        {
+            try
             {
-                components.Dispose();
+                // Setting the flag for 5 minutes and calling the timer setup method for 5 minutes
+                pgbTimerBar.Value = 0;
+                pgbTimerBar.Enabled = true;
+                isFiveMinSelected = true;
+                fiveMinTimer();
+
             }
-            base.Dispose(disposing);
+            catch
+            {
+                // Handling any exceptions that may occur during the timer setup
+                MessageBox.Show("AN ERROR OCCURED");
+                txtTimeStartFinish.Clear();
+                btnFiveMin.Focus();
+            }
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
+        public void fiveMinTimer()
         {
-            components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            lblTimer = new Label();
-            btnFiveMin = new Button();
-            btnTenMin = new Button();
-            btnFifteenMin = new Button();
-            pgbTimerBar = new ProgressBar();
-            txtTimeStartFinish = new TextBox();
-            timeFiveMin = new System.Windows.Forms.Timer(components);
-            timeTenMin = new System.Windows.Forms.Timer(components);
-            timeFifteenMin = new System.Windows.Forms.Timer(components);
-            SuspendLayout();
-            // 
-            // lblTimer
-            // 
-            lblTimer.AutoSize = true;
-            lblTimer.BackColor = Color.Transparent;
-            lblTimer.Font = new Font("Arial Rounded MT Bold", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblTimer.Location = new Point(12, 9);
-            lblTimer.Name = "lblTimer";
-            lblTimer.Size = new Size(636, 39);
-            lblTimer.TabIndex = 0;
-            lblTimer.Text = "Break from studying? Come back in...";
-            // 
-            // btnFiveMin
-            // 
-            btnFiveMin.BackColor = Color.SpringGreen;
-            btnFiveMin.Font = new Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnFiveMin.Location = new Point(12, 83);
-            btnFiveMin.Name = "btnFiveMin";
-            btnFiveMin.Size = new Size(152, 71);
-            btnFiveMin.TabIndex = 0;
-            btnFiveMin.Text = "5 min";
-            btnFiveMin.UseVisualStyleBackColor = false;
-            btnFiveMin.Click += btnFiveMin_Click;
-            // 
-            // btnTenMin
-            // 
-            btnTenMin.BackColor = Color.LightSkyBlue;
-            btnTenMin.Font = new Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnTenMin.Location = new Point(257, 83);
-            btnTenMin.Name = "btnTenMin";
-            btnTenMin.Size = new Size(152, 71);
-            btnTenMin.TabIndex = 1;
-            btnTenMin.Text = "10 min";
-            btnTenMin.UseVisualStyleBackColor = false;
-            btnTenMin.Click += btnTenMin_Click;
-            // 
-            // btnFifteenMin
-            // 
-            btnFifteenMin.BackColor = Color.LightSalmon;
-            btnFifteenMin.Font = new Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnFifteenMin.Location = new Point(491, 83);
-            btnFifteenMin.Name = "btnFifteenMin";
-            btnFifteenMin.Size = new Size(152, 71);
-            btnFifteenMin.TabIndex = 2;
-            btnFifteenMin.Text = "15 min";
-            btnFifteenMin.UseVisualStyleBackColor = false;
-            btnFifteenMin.Click += btnFifteenMin_Click;
-            // 
-            // pgbTimerBar
-            // 
-            pgbTimerBar.ForeColor = Color.Gold;
-            pgbTimerBar.Location = new Point(12, 210);
-            pgbTimerBar.Name = "pgbTimerBar";
-            pgbTimerBar.Size = new Size(631, 35);
-            pgbTimerBar.TabIndex = 3;
-            // 
-            // txtTimeStartFinish
-            // 
-            txtTimeStartFinish.Font = new Font("Arial Rounded MT Bold", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtTimeStartFinish.Location = new Point(210, 282);
-            txtTimeStartFinish.Name = "txtTimeStartFinish";
-            txtTimeStartFinish.ReadOnly = true;
-            txtTimeStartFinish.Size = new Size(224, 36);
-            txtTimeStartFinish.TabIndex = 3;
-            txtTimeStartFinish.TabStop = false;
-            // 
-            // timeFiveMin
-            // 
-            timeFiveMin.Enabled = true;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(655, 333);
-            Controls.Add(txtTimeStartFinish);
-            Controls.Add(pgbTimerBar);
-            Controls.Add(btnFifteenMin);
-            Controls.Add(btnTenMin);
-            Controls.Add(btnFiveMin);
-            Controls.Add(lblTimer);
-            Cursor = Cursors.Cross;
-            Icon = (Icon)resources.GetObject("$this.Icon");
-            Name = "Form1";
-            Text = "Study Break Timer v1.0.1";
-            ResumeLayout(false);
-            PerformLayout();
+            if (isFiveMinSelected)
+            {
+                // Setting Progress Bar Minimum and Maximum values for 5 minutes (2738 ticks)
+                // Starting the timer and stopwatch for 5 minutes
+                pgbTimerBar.Minimum = 0;
+                pgbTimerBar.Maximum = 300;
+                timeFiveMin.Tick += new EventHandler(timerTick);
+                timeFiveMin.Start();
+                stopWatch.Start();
+                // Disabling the buttons to prevent multiple timers from being started simultaneously
+                btnFifteenMin.Enabled = false;
+                btnTenMin.Enabled = false;
+                btnFiveMin.Enabled = false;
+            }
+            else
+            {
+                return;
+            }
         }
 
-        #endregion
+        private void btnTenMin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Setting the flag for 10 minutes and calling the timer setup method for 10 minutes
+                pgbTimerBar.Value = 0;
+                pgbTimerBar.Enabled = true;
+                isTenMinSelected = true;
+                tenMinTimer();
 
-        private Label lblTimer;
-        private Button btnFiveMin;
-        private Button btnTenMin;
-        private Button btnFifteenMin;
-        private ProgressBar pgbTimerBar;
-        private TextBox txtTimeStartFinish;
-        private System.Windows.Forms.Timer timeFiveMin;
-        private System.Windows.Forms.Timer timeTenMin;
-        private System.Windows.Forms.Timer timeFifteenMin;
+            }
+            catch
+            {
+                // Handling any exceptions that may occur during the timer setup
+                MessageBox.Show("AN ERROR OCCURED");
+                txtTimeStartFinish.Clear();
+                btnFiveMin.Focus();
+            }
+        }
+
+        public void tenMinTimer()
+        {
+
+            if (isTenMinSelected)
+            {
+                // Setting Progress Bar Minimum and Maximum values for 10 minutes (5478 ticks)
+                // Starting the timer and stopwatch for 10 minutes  5478
+                pgbTimerBar.Value = 0;
+                pgbTimerBar.Minimum = 0;
+                pgbTimerBar.Maximum = 600;
+                timeTenMin.Tick += new EventHandler(timerTick);
+                timeTenMin.Start();
+                stopWatch.Start();
+                // Disabling the buttons to prevent multiple timers from being started simultaneously
+                btnFifteenMin.Enabled = false;
+                btnTenMin.Enabled = false;
+                btnFiveMin.Enabled = false;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btnFifteenMin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Setting the flag for 15 minutes and calling the timer setup method for 15 minutes
+                pgbTimerBar.Value = 0;
+                pgbTimerBar.Enabled = true;
+                isFifteenMinSelected = true;
+                fifteenMinTimer();
+
+            }
+            catch
+            {
+                // Handling any exceptions that may occur during the timer setup
+                MessageBox.Show("AN ERROR OCCURED");
+                txtTimeStartFinish.Clear();
+                btnFiveMin.Focus();
+            }
+        }
+
+        public void fifteenMinTimer()
+        {
+            if (isFifteenMinSelected)
+            {
+                // Setting Progress Bar Minimum and Maximum values for 15 minutes (8215 ticks)
+                // Starting the timer and stopwatch for 15 minutes 
+                pgbTimerBar.Value = 0;
+                pgbTimerBar.Minimum = 0;
+                pgbTimerBar.Maximum = 900;
+                timeFifteenMin.Tick += new EventHandler(timerTick);
+                timeFifteenMin.Start();
+                stopWatch.Start();
+                // Disabling the buttons to prevent multiple timers from being started simultaneously
+                btnFifteenMin.Enabled = false;
+                btnTenMin.Enabled = false;
+                btnFiveMin.Enabled = false;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            try
+            { 
+                TimeSpan timeSpan = stopWatch.Elapsed;
+
+
+                // Incrementing the progress bar value and updating the time display until the maximum value is reached
+                if (pgbTimerBar.Value < pgbTimerBar.Maximum)
+                {
+                    txtTimeStartFinish.Text = String.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
+                    pgbTimerBar.Value = (int)timeSpan.TotalSeconds;
+                }
+                else
+                {
+                    // Stopping the timers and stopwatch, resetting the display and progress bar, and showing a message when the break is finished
+                    timeFiveMin.Stop();
+                    timeTenMin.Stop();
+                    timeFifteenMin.Stop();
+                    stopWatch.Stop();
+                    txtTimeStartFinish.Text = "FINISH";
+                    stopWatch.Reset();
+                    this.TopMost = true;
+                    MessageBox.Show("BREAK FINISHED! Time to study!");
+                    btnFifteenMin.Enabled = true;
+                    btnTenMin.Enabled = true;
+                    btnFiveMin.Enabled = true;
+                    pgbTimerBar.Enabled = false;
+                    pgbTimerBar.Invalidate();
+                    this.TopMost = false;
+                }
+            }
+            catch
+            {
+                // Handling any exceptions that may occur during the timer tick event
+                MessageBox.Show("AN ERROR OCCURED");
+                txtTimeStartFinish.Clear();
+                btnFiveMin.Focus();
+            }
+
+        }
     }
 }
